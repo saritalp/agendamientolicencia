d05555026a0489c33052128efa6383b784fe13cd
saritalp <saritalondonop@gmail.com>
2024-08-07 11:44:49 -0500

    Commit1

---
+++ src/main/java/co/edu/uniquindio/poo/model/Cliente.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.poo.model;
+import lombok.AllArgsConstructor;
+
+import lombok.Getter;
+@AllArgsConstructor
+@Getter 
+public class Cliente {
+    private String nombre;
+    private String ID;
+    private String email;
+    private String telefono;
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module co.edu.uniquindio.poo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires lombok;
 
     opens co.edu.uniquindio.poo to javafx.fxml;
     exports co.edu.uniquindio.poo;
